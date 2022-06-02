93e0e745d3d582995eaf7a84ec0fa51338582d82
jcampbell <jcampbell57@gmail.com>
2022-06-02 11:07:02 -0700

    Added playRound function and all possible outcomes.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-console.log("Hello world!");
-
 let  selection = [ "Boot", "Cockroach", "Nuclear Bomb" ]
 
 function computerPlay(selection) {
@@ -7,4 +5,31 @@ function computerPlay(selection) {
    return selection[index];
 }  
 
-console.log(computerPlay(selection));
+let computerSelection = computerPlay(selection);
+console.log(computerSelection)
+
+let playerSelection = "Cockroach";
+
+function playRound (playerSelection, computerSelection) {
+   if (playerSelection == computerSelection) {
+      return "Tie!"
+   } else if (playerSelection == "Boot" && computerSelection == "Cockroach") {
+      return "You win! Boot beats Cockroach"
+   } else if (playerSelection == "Boot" && computerSelection == "Nuclear Bomb") {
+      return "You lose! Nuclear Bomb beats Boot"
+   } else if (playerSelection == "Cockroach" && computerSelection == "Nuclear Bomb") {
+      return "You win! Cockroach beats Nuclear Bomb"
+   } else if (playerSelection == "Cockroach" && computerSelection == "Boot") {
+      return "You lose! Boot beats Cockroach"
+   } else if (playerSelection == "Nuclear Bomb" && computerSelection == "Boot") {
+      return "You win! Nuclear Bomb beats Boot"
+   } else if (playerSelection == "Nuclear Bomb" && computerSelection == "Cockroach") {
+      return "You lose! Cockroach beats Nuclear Bomb"
+   } else {
+      return "Something went horribly wrong!"
+   }
+} 
+
+let result = playRound(playerSelection, computerSelection);
+
+console.log(result);
